907a60489c800ead4a3888699e07522cde41b1d8
Sayan Roy <100078622+RoyLays@users.noreply.github.com>
2024-09-08 18:17:48 +0000

    conversionOparations

---
+++ 01_basics/03_conversions.js
@@ -0,0 +1,15 @@
+score = 33
+
+console.log(typeof score)
+
+score2 = "34"
+score3 = "35abc"
+
+let convNum1 = Number(score2)
+let convNum2 = Number(score3)
+
+console.log(typeof score2,typeof score3);
+console.log(typeof convNum1);
+console.log(convNum1);
+console.log(typeof convNum2);
+console.log(convNum2);
