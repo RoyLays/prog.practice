fedac36206f69eacc76b3fcb66026848e9352587
Sayan Roy <100078622+RoyLays@users.noreply.github.com>
2024-09-08 07:16:45 +0000

    datatypes

---
+++ 01_basics/test2.js
@@ -0,0 +1,15 @@
+const accountID = 1445
+let accEmail = "sr@lays.in"
+var accPasskey = "123Roy45"
+let newNum;
+num = 6
+
+// accountID = 458
+accPasskey = "123SR45"
+console.log(accountID, accEmail, accPasskey , newNum)
+console.table([accountID, accEmail, accPasskey , newNum])
+
+console.log(typeof "lays")
+console.log(typeof num)
+console.log(typeof null)
+console.log(typeof undefine)
